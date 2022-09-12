b8f21fd4dece512cded4da31690a233e2b78b03f
zhangchaoqing <zzccqq2016@126.com>
2022-09-12 20:10:43 +0800

    commit

--- src/main/java/org/kira/learn/springsecurity/oauth2/resource/ResourceApplication.java
+++ src/main/java/org/kira/learn/springsecurity/oauth2/resource/ResourceApplication.java
@@ -2,11 +2,13 @@ package org.kira.learn.springsecurity.oauth2.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 /**
  * @author: Zhang Chaoqing
  * Date: 2022/8/30 23:40
  */
+@EnableOAuth2Sso
 @SpringBootApplication
 public class ResourceApplication {
     public static void main(String[] args) {
